b7bea572c0c2310ed4d25ee52c314c2d94ef0f1f
Bikstok <jkjaerulff@gmail.com>
2025-08-28 11:00:27 +0200

    Added the misc folder and variables

---
+++ 01._Miscellaneous/variables_1.js
@@ -0,0 +1,23 @@
+// use const if possible
+// otherwise use let
+
+//String, Boolean, Number, BigInt, Object, Symbol, undefinded, null
+
+
+// node <file_name>
+
+
+console.log("deez nuts")
+
+
+let a = "jakob"
+console.log(a)
+
+console.log(`hello ${a}`, 2+2);
+
+
+
+// Javascript object
+const person = {
+    name: "Jakob" // key-value pair
+}
